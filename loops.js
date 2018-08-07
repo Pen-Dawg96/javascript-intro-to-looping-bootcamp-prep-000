5dbb6ebaecececca4f34de310df9bd3c969a917c
Penny Cornell <penny.cornell1@hotmail.com>
2018-08-07 16:44:08 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,12 @@
+function forLoop(array){
+  for (let i = 0; i < 25; i++) {
+    console.log(`I am ${i} strange loops.`)
+  }
+  return array
+}
+function whileLoop(number){
+  let countdown = 100;
+  while (countdown > 0){
+    console.log(--countdown)
+   }
+}
